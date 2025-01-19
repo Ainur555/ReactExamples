36972ed11f023b78993e0a4aaff78f7bd2ab0457
ainur <ainur@AINURPC>
2025-01-19 12:46:13 +0300

    'Fixes'

--- src/components/MessageBox.jsx
+++ src/components/MessageBox.jsx
@@ -1,39 +1,11 @@
 import React from "react";
+import ErrorBox from "./ErrorBox";
+import DataList from "./DataList";
 
 const MessageBox = ({ err, data }) => (
     <div>
-        {/* Отображение ошибки */}
-        {err && (
-            <div style={{ backgroundColor: 'red', color: 'white', padding: '10px', marginTop: '10px' }}>
-                {err}
-            </div>
-        )}
-
-        {/* Отображение данных (списка или строки) */}
-        {data && Array.isArray(data) ? (
-        <ol 
-            style={{
-                backgroundColor: 'green', 
-                color: 'white', 
-                padding: '10px', 
-                marginTop: '10px', 
-                listStyleType: 'decimal',  
-                paddingLeft: '20px',  
-            }}
-            >
-        {data.map((item, index) => (
-                    <li key={index} style={{ marginBottom: '5px' }}>
-                        {item.fact}
-                    </li>
-                ))}
-            </ol>
-        ) : (
-            data && (
-                <div style={{ backgroundColor: 'green', color: 'white', padding: '10px', marginTop: '10px' }}>
-                    {data}
-                </div>
-            )
-        )}
+        <ErrorBox message={err} />
+        <DataList data={data} />
     </div>
 );
 
--- src/components/RequestButton.jsx
+++ src/components/RequestButton.jsx
@@ -1,41 +1,55 @@
 import React, { useState } from "react";
 import MessageBox from "./MessageBox";
+import axios from "axios";
+import  '../styles/RequestButton.css';
 
 const urlAddressCorrect = "https://catfact.ninja/facts";
 const urlAddressNotCorrect = "https://catfact.ninja/facts12123";
 
 const RequestButton = () => {
-    const [facts, setFacts] = useState(null); // список фактов
-    const [error, setError] = useState(null); // error message
+    const [facts, setFacts] = useState(null); 
+    const [error, setError] = useState(null); 
+    const [isLoading, setIsLoading] = useState(false);
 
     const requestToApi = async (urlAddress) => {
         setError(null);
-        setFacts(null);
-        try{
-            const response = await fetch( urlAddress, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json',
-                },
-            });
-            if (!response.ok) {
-                throw new Error(`Ошибка: ${response.status}`);
-            }
-            const data = await response.json();
-            setFacts(data.data); // сохраняем список фактов
-        } catch (err) {
-            setError(err.message);
-        }
+        setFacts(null);       
+        setIsLoading(true);
+
+        await axios.get(urlAddress)
+        .then(response => {
+            setFacts(response.data.data);
+        })
+        .catch(error => {
+            setError(error.message);
+        })
+        .finally(() => {
+            setIsLoading(false); 
+        })};
+        return (
+            <div>
+                <button
+                onClick={() => requestToApi(urlAddressCorrect)}
+                disabled={isLoading}
+                className="request-button"
+                >
+                {isLoading ? "Загрузка..." : "Корректный запрос к API"}
+                </button>
+                <button
+                onClick={() => requestToApi(urlAddressNotCorrect)}
+                disabled={isLoading}
+                className="request-button"
+                >
+                {isLoading ? "Загрузка..." : "Запрос к API c ошибкой"}
+                </button>
+        
+                {isLoading && <div className="spinner"></div>}
+                {isLoading && <p className="loading-message">Данные загружаются...</p>}
+                <MessageBox err={error} data={facts} />
+            </div>
+            );
     };
 
-    return (
-        <div>
-             <button onClick={() => requestToApi('https://catfact.ninja/facts')}  style={{marginRight: '20px', fontSize: '18px'}}>Корректный запрос к API</button> 
-             <button onClick={() => requestToApi(urlAddressNotCorrect)} style={{marginLeft:  '20px', fontSize: '18px'}}>Запрос к API с ошибкой</button> 
-             <MessageBox err={error} data = {facts}/> 
-        </div>
-    );
-};
-
+        
+    
 export default RequestButton;
